244b150f79ef57faab428f779ff7ecab69df39ca
Fatimah Sharafudeen <fatimah.sharafudeen@gmail.com>
2025-05-28 12:55:03 +0100

    Added 'h1' to the header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import logo from './logo.svg';
+import axios from 'axios';
+import { RotatingLines } from 'react-loader-spinner';
 import './App.css';
 
 function App() {
@@ -6,17 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My Page</h1>
       </header>
     </div>
   );
